aaadca6848a3430d6e4f36a6b34207d56d0aa5c8
Michael R. Welch <welchm2@wit.edu>
2014-12-11 00:06:27 -0500

    Added the Test Package.

---
+++ SnakeSemesterProject/src/SnakeGame_Test/Apple.java
@@ -0,0 +1,14 @@
+package SnakeGame_Test;
+
+import ch.aplu.jgamegrid.*;
+
+public class Apple extends Actor {
+	
+	/**
+	 * Defines the image of the Apple class.
+	 */
+	public Apple() {
+		//Constructs an unrotatable actor based on the specified sprite image.
+		super("sprites/apple_small_18.png");
+	}
+}
---
+++ SnakeSemesterProject/src/SnakeGame_Test/Test_SnakeGame.java
@@ -0,0 +1,12 @@
+package SnakeGame_Test;
+
+public class Test_SnakeGame {
+	public Test_SnakeGame() {
+	}
+
+	public static void main(String[] args){
+		Test_GameWindow test0 = new Test_GameWindow();
+		test0.setTitle("Test to create GameWindow");
+	}
+
+}
